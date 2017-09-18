051e4193682dfd1eb6db0282c61ea03d8422a8ed
WYC <wycstar@live.com>
2017-09-18 18:23:02 +0800

    修改分页功能

--- server/config.py
+++ server/config.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import sys
+import os
+import json
+
 
 class Config(object):
+    real_path = os.path.split(os.path.realpath(sys.argv[0]))[0]
+    with open(real_path + os.sep + 'config.json') as f:
+        c = json.loads(f.read())
     SECRET_KEY = 'asdghtyu'
     WTF_CSRF_SECRET_KEY = 'acf1235'
     WTF_CSRF_ENABLED = True
+    DOMAIN = c['domain']
 
 
 class OnlineConfig(Config):
--- server/view.py
+++ server/view.py
@@ -22,13 +22,15 @@ def search_result(keyword, page_num):
     if query_form.validate_on_submit():
      return redirect(url_for('search_result', keyword=query_form.keyword.data, page_num=1))
     return render_template('result.html',
-                            query_form=query_form,
-                            keyword=keyword,
-                            result_num=r.get('total') if r is not None else 0,
-                            result_time=r.get('took') if r is not None else 0,
-                            results=r.get('result') if r is not None else [],
-                            current_page=page_num,
-                            sort_type=request.args.get('sort'))
+                           domain=SITE.config.get('DOMAIN'),
+                           query_form=query_form,
+                           keyword=keyword,
+                           # result_num=r.get('total') if r is not None else 0,
+                           result_num=400,
+                           result_time=r.get('took') if r is not None else 0,
+                           results=r.get('result') if r is not None else [],
+                           current_page=page_num,
+                           sort_type=request.args.get('sort'))
 
 
 @SITE.route('/hash/<h>', methods=['GET'])
